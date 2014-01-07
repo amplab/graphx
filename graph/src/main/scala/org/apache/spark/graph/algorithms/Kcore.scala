0f06aedbe2c4f08c560585534b6c1f3072c269a7
Dan Crankshaw <dscrankshaw@gmail.com>
2014-01-07 14:38:03 -0800

    Added documentation to kcore.

--- graph/src/main/scala/org/apache/spark/graph/algorithms/Kcore.scala
+++ graph/src/main/scala/org/apache/spark/graph/algorithms/Kcore.scala
@@ -4,42 +4,46 @@ import org.apache.spark.graph._
 import org.apache.spark._
 import scala.math._
 
-
 object KCore extends Logging {
+  /**
+   * Compute the k-core decomposition of the graph for all k <= kmax. This
+   * uses the iterative pruning algorithm discussed by Alvarez-Hamelin et al.
+   * in K-Core Decomposition: a Tool For the Visualization of Large Scale Networks
+   * (see <a href="http://arxiv.org/abs/cs/0504107">http://arxiv.org/abs/cs/0504107</a>).
+   *
+   * @tparam VD the vertex attribute type (discarded in the computation)
+   * @tparam ED the edge attribute type (preserved in the computation)
+   *
+   * @param graph the graph for which to compute the connected components
+   * @param kmax the maximum value of k to decompose the graph
+   *
+   * @return a graph where the vertex attribute is the minimum of
+   * kmax or the highest value k for which that vertex was a member of
+   * the k-core.
+   *
+   * @note This method has the advantage of returning not just a single kcore of the
+   * graph but will yield all the cores for all k in [1, kmax].
+   */
 
   def run[VD: Manifest, ED: Manifest](
       graph: Graph[VD, ED],
-      kmin: Int = 1,
       kmax: Int)
     : Graph[Int, ED] = {
 
     // Graph[(Int, Boolean), ED] - boolean indicates whether it is active or not
     var g = graph.outerJoinVertices(graph.degrees)((vid, oldData, newData) => (newData.getOrElse(0), true))
-
     var curK = 1
     while (curK <= kmax) {
       g = computeCurrentKCore(g, curK)
-      logWarning("grepthisxxx curK: " + curK + g.vertices.collect.deep.mkString(" "))
-      // all vertices still on are part of curK-core, all vertices turned off are members of the shell
-      // that their vertex data indicates
       curK += 1
     }
-    
-    // g.mapVertices({ case (_, (k, _)) => min(k, curK)})
-    //   .subgraph({x => true}, {(v, d) => d >= kmin})
-
     g.mapVertices({ case (_, (k, _)) => k})
-
   }
-
-
-
 
   def computeCurrentKCore[ED: Manifest](graph: Graph[(Int, Boolean), ED], k: Int) = {
     def sendMsg(et: EdgeTriplet[(Int, Boolean), ED]): Iterator[(Vid, (Int, Boolean))] = {
-      // check if either vertex has already been turned off, in which case we do nothing
       if (!et.srcAttr._2 || !et.dstAttr._2) {
-        // TODO(crankshaw) - need this case for the first round of Pregel for k > 1
+        // if either vertex has already been turned off, in which case we do nothing
         Iterator.empty
       } else if (et.srcAttr._1 < k && et.dstAttr._1 < k) {
         // tell both vertices to turn off but don't need change count value
@@ -51,33 +55,16 @@ object KCore extends Logging {
         // if dst is being pruned, tell src to subtract from vertex count but don't turn off
         Iterator((et.dstId, (0, false)), (et.srcId, (1, true)))
       } else {
-        // no changes required
         Iterator.empty
       }
     }
 
     // subtracts removed neighbors from neighbor count and tells vertex whether it was turned off or not
     def mergeMsg(m1: (Int, Boolean), m2: (Int, Boolean)): (Int, Boolean) = {
-      // val count = (max(k - 1, m1._1 + m2._1))
-      // val on = m1._2 + m2._2
-      // (count, on)
-
-      // because the vertex count doesn't change within a round, all edgetriplets for which
-      // this vertex is part of should either tell the vertex to be on or off, so the messages
-      // should indicate the same state
-      if (m1._2 != m2._2) {
-        logWarning("KCORE: merged messages indicate different vertex states")
-      }
-
-      // compute number of removed neighbors and determine whether vertex is on or off
       (m1._1 + m2._1, m1._2 && m2._2)
     }
 
     def vProg(vid: Vid, data: (Int, Boolean), update: (Int, Boolean)): (Int, Boolean) = {
-      // If the count drops below k, then the vertex will be turned off in the next round anyway
-      // This *should* make sure that if the vertex has been turned off, then it will be recorded as
-      // having been turned off when pruning the kth shell, and thus at the end we know the highest kcore
-      // it was part of was for k-1
       var newCount = data._1
       var on = data._2
       if (on) {
@@ -90,5 +77,4 @@ object KCore extends Logging {
     // Note that initial message should have no effect
     Pregel.undirectedRun(graph, (0, true))(vProg, sendMsg, mergeMsg)
   }
-
 }
